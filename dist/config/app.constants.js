7a2652c6c7c74776eff77c49ac87b4124b1a81f6
davieoba <bodunrindavidbond@gmail.com>
2024-07-07 22:13:07 +0100

    FEAT: implemented api -> a user can be added to an organization

--- dist/config/app.constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const APP_CONSTANTS = {
-    USER_LABEL: "user",
-    USER_PASSWORD_LABEL: "user_password",
-    LOGIN_SESSION_LABEL: "login_session",
-};
-exports.default = APP_CONSTANTS;
