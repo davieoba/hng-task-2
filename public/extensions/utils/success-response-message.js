781bbb314033d799e3878c84feb26a512e7a745a
davieoba <bodunrindavidbond@gmail.com>
2024-07-09 09:17:49 +0100

    update

--- public/extensions/utils/success-response-message.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SUCCESS_RESPONSE_MESSAGE = Object.freeze({
-    SIGNUP_SUCCESSFUL: "Registation successful",
-    LOGIN_SUCCESSFUL: "Login successful",
-});
-exports.default = SUCCESS_RESPONSE_MESSAGE;
